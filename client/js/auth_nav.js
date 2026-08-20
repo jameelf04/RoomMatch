0e33c30d1508d5f6b0735f2669a1e5fc4f3f9087
Jameel Fakhoury <jameel.fakhoury74@gmail.com>
2026-08-20 17:33:50 +0300

    dynamic auth nav with logout

---
+++ client/js/auth_nav.js
@@ -0,0 +1,29 @@
+const authNav = document.querySelector(".auth_nav");
+const token = localStorage.getItem("token");
+const username = localStorage.getItem("username");
+
+const pagePrefix = window.location.pathname.includes("/pages/") ? "" : "client/pages/";
+
+if (token) {
+    authNav.innerHTML = `
+        <span class="nav_user">Hi, ${username}</span>
+        <a href="#" id="logout_btn">Log Out</a>
+    `;
+
+    const logoutBtn = document.getElementById("logout_btn");
+    logoutBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        localStorage.removeItem("token");
+        localStorage.removeItem("username");
+        if (pagePrefix == "") {
+            window.location.href = "../../index.html";
+        } else {
+            window.location.href = "index.html";
+        }
+    });
+} else {
+    authNav.innerHTML = `
+        <a href="${pagePrefix}login.html">Log In</a>
+        <a href="${pagePrefix}signup.html" class="filled">Sign Up</a>
+    `;
+}
